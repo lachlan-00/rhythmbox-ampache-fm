0ad7fc5c86a94a5968e539d7ed60efc6803e68b7
lachlan <lachlan.00@gmail.com>
2019-10-22 12:25:48 +1000

    Update ampache.py
    add stream and download

--- ampache.py
+++ ampache.py
@@ -21,6 +21,7 @@ Ampache XML-Api v400001 for python3
 """
 
 import hashlib
+import requests
 import time
 import urllib.parse
 import urllib.request
@@ -809,21 +810,17 @@ def url_to_song(ampache_url, ampache_api, url):
     * ampache_url = (string)
     * ampache_api = (string)
     * exact
-    * add
-    * update
     * filter
     * offset
     * limit
 """
-def playlists(ampache_url, ampache_api, exact, add, update, filter, offset, limit):
+def playlists(ampache_url, ampache_api, exact = '', filter = '', offset = 0, limit = 0):
     if not ampache_url or not ampache_api:
         return False
     ampache_url = ampache_url + '/server/xml.server.php'
     data = urllib.parse.urlencode({'action': 'playlists',
                                    'auth': ampache_api,
                                    'exact': exact,
-                                   'add': add,
-                                   'update': update,
                                    'filter': filter,
                                    'offset': str(offset),
                                    'limit': str(limit)})
@@ -888,7 +885,7 @@ def playlist(ampache_url, ampache_api, filter):
     * offset
     * limit
 """
-def playlist_songs(ampache_url, ampache_api, filter, offset, limit):
+def playlist_songs(ampache_url, ampache_api, filter, offset = 0, limit = 0):
     if not ampache_url or not ampache_api:
         return False
     ampache_url = ampache_url + '/server/xml.server.php'
@@ -1773,3 +1770,52 @@ def update_from_tags(ampache_url, ampache_api, ampache_type, ampache_id):
         token = False
     return token
 
+""" stream
+    MINIMUM_API_VERSION=400001
+
+    stream a song or podcast episode
+
+    INPUTS
+    * ampache_url = (string)
+    * ampache_api = (string)
+    * id          = (string) $song_id / $podcast_episode_id
+    * type        = (string) 'song'|'podcast'
+    * destination = (string) full file path)
+"""
+def stream(ampache_url, ampache_api, id, type, destination):
+    if not ampache_url or not ampache_api or not user:
+        return False
+    ampache_url = ampache_url + '/server/xml.server.php'
+    data = urllib.parse.urlencode({'action': 'stream',
+                                   'auth': ampache_api,
+                                   'id': id,
+                                   'type': type})
+    full_url = ampache_url + '?' + data
+    result = requests.get(full_url, allow_redirects=True)
+    open(destination, 'wb').write(result.content)
+    return True
+
+""" download
+    MINIMUM_API_VERSION=400001
+
+    download a song or podcast episode
+
+    INPUTS
+    * ampache_url = (string)
+    * ampache_api = (string)
+    * id          = (string) $song_id / $podcast_episode_id
+    * type        = (string) 'song'|'podcast'
+    * destination = (string) full file path)
+"""
+def download(ampache_url, ampache_api, id, type, destination):
+    if not ampache_url or not ampache_api or not user:
+        return False
+    ampache_url = ampache_url + '/server/xml.server.php'
+    data = urllib.parse.urlencode({'action': 'download',
+                                   'auth': ampache_api,
+                                   'id': id,
+                                   'type': type})
+    full_url = ampache_url + '?' + data
+    result = requests.get(full_url, allow_redirects=True)
+    open(destination, 'wb').write(result.content)
+    return True
